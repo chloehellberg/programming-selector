6f3b35162fd4756dd7c3eb3f42c6e45c07cfa181
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-12 18:51:34 -0700

    Add initial JS to program

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//   $("form#question1").submit(function(event) {
+//     const true = parseInt($("input#true").val());
+//     const false = parseInt($("input#false").val());
+  
+//     let result1;
+    
+//     if (true) {            
+//       alert('Try Javascript!');
+//     }
+//     else (false) {
+//      alert('Perhaps C# or Python are more suitable.')
+//     }
+    
+//     event.preventDefault();
+//   });
+// });
+
+
+
+//  function doyoulikeJavaScript() {
+//    var string;
+//   if (true) {
+//     alert = "You should try Javascript!";
+//   } else {
+//     alert = "Perhaps C# or Python are more your thing."
+//   }
+// }
