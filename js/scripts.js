98b9b2e663521012e41bea3aeb6e80dfab5b73c4
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-13 22:10:18 -0700

    add if else logic to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,31 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
-    const question1 = parseInt($("input:radio[name=question1]:checked").val());
-    const question2 = parseInt($("input:radio[name=question2]:checked").val());
-    const question3 = parseInt($("input:radio[name=question3]:checked").val());
-    const question4 = parseInt($("input:radio[name=question4]:checked").val());
-    const question5 = parseInt($("input:radio[name=question5]:checked").val());
-    const question6 = parseInt($("input:radio[name=question6]:checked").val());
-  })
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-//   $("form#question1").submit(function(event) {
-//     const true = parseInt($("input#true").val());
-//     const false = parseInt($("input#false").val());
-  
-//     let result1;
+    event.preventDefault();
+    const question1 = $("input:radio[name=question1]:checked").val();
+    const question2 = $("input:radio[name=question2]:checked").val();
+    const question3 = $("input:radio[name=question3]:checked").val();
+    const question4 = $("input:radio[name=question4]:checked").val();
+    const question5 = $("input:radio[name=question5]:checked").val();
+    const question6 = $("input:radio[name=question6]:checked").val();
     
-//     if (true) {            
-//       alert('Try Javascript!');
-//     }
-//     else (false) {
-//      alert('Perhaps C# or Python are more suitable.')
-//     }
-    
-//     event.preventDefault();
-//   });
-// });
+    // console.log(question1);
 
+    let result;
 
+     if (question1 === "true") {
+       result = "Javascript"
+     }
+      else if (question1 == "false" && question2 == "true") {
+       result = "Python"
+     }
+      else if (question2 == "false" && question3 == "false") {
+      result = "C#"
+    }
+      else {
+        result = "Javascript"
+      }
+    console.log(result);
+   $("#output").text(result);
+  });
+});
 
-//  function doyoulikeJavaScript() {
-//    var string;
-//   if (true) {
-//     alert = "You should try Javascript!";
-//   } else {
-//     alert = "Perhaps C# or Python are more your thing."
-//   }
-// }
