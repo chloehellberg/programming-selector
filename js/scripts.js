6fbe2ecf55497f34d3baf4fb1da03fe3a520c239
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-13 20:38:45 -0700

    Add question variables to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    const question1 = parseInt($("input:radio[name=question1]:checked").val());
+    const question2 = parseInt($("input:radio[name=question2]:checked").val());
+    const question3 = parseInt($("input:radio[name=question3]:checked").val());
+    const question4 = parseInt($("input:radio[name=question4]:checked").val());
+    const question5 = parseInt($("input:radio[name=question5]:checked").val());
+    const question6 = parseInt($("input:radio[name=question6]:checked").val());
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 // $(document).ready(function() {
 //   $("form#question1").submit(function(event) {
 //     const true = parseInt($("input#true").val());
